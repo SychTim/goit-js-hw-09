c9cc7ea9f1170d8f38f72f4bb9fe1cdbb93cab5c
SychTim <103521623+SychTim@users.noreply.github.com>
2024-06-23 22:15:12 +0200

    finaly resoult

---
+++ src/js/2-form.js
@@ -0,0 +1,36 @@
+let formData = { email: "", message: "" };
+const form = document.querySelector(".feedback-form");
+
+if (localStorage.getItem("feedback-form-state")) {
+    formData.email = JSON.parse(localStorage.getItem("feedback-form-state")).email;
+    formData.message = JSON.parse(localStorage.getItem("feedback-form-state")).message;
+    form.email.value = formData.email;
+    form.message.value = formData.message;
+}
+
+form.addEventListener('input', formUpdate);
+
+function formUpdate (evt) {
+    if (evt.target.type === "email") {
+        formData.email = evt.target.value;
+    } else {
+        formData.message = evt.target.value;
+    };
+
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+}
+
+form.addEventListener('submit', formSubmit);
+
+function formSubmit(evt) {
+    evt.preventDefault();
+    if (evt.currentTarget.message.value === '' || evt.currentTarget.email.value === '') {
+        return;
+    }
+
+    localStorage.removeItem("feedback-form-state");
+    formData.email = '';
+    formData.message = '';
+    form.email.value = '';
+    form.message.value = '';
+}
